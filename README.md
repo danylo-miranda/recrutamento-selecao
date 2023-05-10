a2b4db3868b51c94a32b72f8a7b2f7820bd6e7fb
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-09 22:04:33 -0300

    Update 1.0
    Adição da lista com nome dos candidatos e notas dos testes aplicados pelos recrutadores.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# recrutamento-selecao
-Criar com Python uma lista para armazenar esses resultados (e outros resultados que quiser no mesmo formato, o código precisa funcionar para qualquer lista que seja inserida nesse formato) e depois uma função que busca o candidato de acordo com os critérios digitados pelo usuário.
+# Recrutamento & Selecao
+---
+
+* Lista para armazenar os resultados 
+* Função que busca o candidato de acordo com os critérios digitados pelo usuário.
+
+---
--- sistema.py
+++ sistema.py
@@ -1,1 +1,31 @@
+resultado = []
+#ESTRUTURA DE REPETIÇÃO WHILE: o usuário posde inserir quantos candidatos quiser
+while True:
+    candidato = str(input('nome do candidato: '\
+        'Digite "fim" para encerrar'))
+    if candidato == 'fim': 
+        break
+  
+    nota_entrevista = float(input('nota da entrevista: '))
+    while nota_entrevista < 0 or nota_entrevista > 10.1:
+        print('insira uma nota válida entre 0 e 10')
+        nota_entrevista = float(input('nota da entrevista: '))
+           
+    nota_teorico = float(input('nota do teste teórico: '))
+    while nota_teorico < 0 or nota_teorico > 10.1:
+        print('insira uma nota válida entre 0 e 10')
+        nota_teorico = float(input('nota do teste teórico: '))
+        
+    nota_pratica = float(input('nota do teste prático: '))
+    while nota_pratica < 0 or nota_pratica > 10.1:
+        print('insira uma nota válida entre 0 e 10')
+        nota_pratica = float(input('nota do teste prático: '))
+         
+    nota_soft = float(input('nivel de soft skill: '))
+    while nota_soft < 0 or nota_soft > 10.1:
+        print('insira uma nota válida entre 0 e 10')
+        nota_soft = float(input('nivel de soft skill: '))
+        
+    resultado.append({'candidato':candidato, 'entrevista':nota_entrevista, 'prova teorica':nota_teorico, 'prova pratica':nota_pratica, 'soft skill':nota_soft}) 
+print(resultado)
 
