3adb3802dd5a5f5b1e499b19f3e80d05001a3cbb
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-11 08:35:48 -0300

    Update 1.0.1
    Notas atualização 1.0.1
    
    * Menu de opções:
    adicionei um menu de opções para que o usuário tenha uma melhor interação com o sistema.
    
    * Criação de listas:
    adicionei uma lista individual para cada informação necessária para a execução do programa.
    
    *Resultado
    uma variavel criada para concatenar as listas

--- sistema.py
+++ sistema.py
@@ -1,31 +1,51 @@
-resultado = []
+lista_candidato = []
+entrevista = []
+teorica = []
+pratica = []
+soft = []
+
 #ESTRUTURA DE REPETIÇÃO WHILE: o usuário posde inserir quantos candidatos quiser
 while True:
-    candidato = str(input('nome do candidato: '\
-        'Digite "fim" para encerrar'))
-    if candidato == 'fim': 
-        break
-  
-    nota_entrevista = float(input('nota da entrevista: '))
-    while nota_entrevista < 0 or nota_entrevista > 10.1:
-        print('insira uma nota válida entre 0 e 10')
-        nota_entrevista = float(input('nota da entrevista: '))
+        opcoes = input(
+        f"\nBem Vindo(a)\n"
+        "[1] Cadastrar Candidato\n"
+        "[2] Listar como Tabela\n"
+        "[3] Resultados\n"
+        "[4] Encerrar o Sistema\n"
+        "Escolha uma das opções:"
+    )
+        
+        if opcoes == '4': 
+            break
+        
+        elif opcoes == '1':
+            candidato = str(input('nome do candidato: '))
+            nota_entrevista = float(input('nota da entrevista: '))
+            while nota_entrevista < 0 or nota_entrevista > 10.1:
+                print('insira uma nota válida entre 0 e 10')
+                nota_entrevista = float(input('nota da entrevista: '))
            
-    nota_teorico = float(input('nota do teste teórico: '))
-    while nota_teorico < 0 or nota_teorico > 10.1:
-        print('insira uma nota válida entre 0 e 10')
-        nota_teorico = float(input('nota do teste teórico: '))
+            nota_teorico = float(input('nota do teste teórico: '))
+            while nota_teorico < 0 or nota_teorico > 10.1:
+                print('insira uma nota válida entre 0 e 10')
+                nota_teorico = float(input('nota do teste teórico: '))
         
-    nota_pratica = float(input('nota do teste prático: '))
-    while nota_pratica < 0 or nota_pratica > 10.1:
-        print('insira uma nota válida entre 0 e 10')
-        nota_pratica = float(input('nota do teste prático: '))
+            nota_pratica = float(input('nota do teste prático: '))
+            while nota_pratica < 0 or nota_pratica > 10.1:
+                print('insira uma nota válida entre 0 e 10')
+                nota_pratica = float(input('nota do teste prático: '))
          
-    nota_soft = float(input('nivel de soft skill: '))
-    while nota_soft < 0 or nota_soft > 10.1:
-        print('insira uma nota válida entre 0 e 10')
-        nota_soft = float(input('nivel de soft skill: '))
+            nota_soft = float(input('nivel de soft skill: '))
+            while nota_soft < 0 or nota_soft > 10.1:
+                print('insira uma nota válida entre 0 e 10')
+                nota_soft = float(input('nivel de soft skill: '))
         
-    resultado.append({'candidato':candidato, 'entrevista':nota_entrevista, 'prova teorica':nota_teorico, 'prova pratica':nota_pratica, 'soft skill':nota_soft}) 
+            lista_candidato.append({'candidato':candidato})
+            entrevista.append({'entrevista':nota_entrevista})
+            teorica.append({'prova teorica':nota_teorico})
+            pratica.append({'prova pratica':nota_pratica})
+            soft.append({'soft skill':nota_soft}) 
+    
+resultado = lista_candidato + entrevista + teorica + pratica + soft
 print(resultado)
 
