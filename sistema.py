025bd3ae346691fb3eba001906bc6368d98d9981
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-13 06:30:38 -0300

    Update 1.0.3
    Simplificação do codigo

--- sistema.py
+++ sistema.py
@@ -1,66 +1,21 @@
-lista_candidato = []
-
 def listar_resultados(entrevista, teorico, pratica, soft):
-    resultados = [lista_candidato]
     candidatos_aprovados = []
-    for resultado in resultados:
-        if resultado[1] >= entrevista and resultado[2] >= teorico and resultado[3] >= pratica and resultado[4] >= soft:
-            candidatos_aprovados.append(resultado)
-            
-    
+    for i in lista_candidato:
+        if lista_candidato[0][1] >= entrevista and lista_candidato[0][2] >= teorico and lista_candidato[0][3] >= pratica and lista_candidato[0][4] >= soft:
+            candidatos_aprovados.append(i)
     return candidatos_aprovados
 
-#ESTRUTURA DE REPETIÇÃO WHILE: o usuário posde inserir quantos candidatos quiser
-while True:
-        opcoes = input(
-        f'\nMENU PRINCIPAL\n'
-        '[1] Cadastrar Candidato\n'
-        '[2] Listar resultados\n'
-        '[3] Encerrar o Sistema\n'
-        'Escolha uma das opções:'
-    )
-        
-        if opcoes == '3': 
-            break
-        
-        elif opcoes == '1':
-            candidato = str(input('nome do candidato: '))
-            nota_entrevista = float(input('nota da entrevista: '))
-            while nota_entrevista < 0 or nota_entrevista > 10:
-                print('insira uma nota válida entre 0 e 10')
-                nota_entrevista = float(input('nota da entrevista: '))
-           
-            nota_teorico = float(input('nota do teste teórico: '))
-            while nota_teorico < 0 or nota_teorico > 10:
-                print('insira uma nota válida entre 0 e 10')
-                nota_teorico = float(input('nota do teste teórico: '))
-        
-            nota_pratica = float(input('nota do teste prático: '))
-            while nota_pratica < 0 or nota_pratica > 10:
-                print('insira uma nota válida entre 0 e 10')
-                nota_pratica = float(input('nota do teste prático: '))
-         
-            nota_soft = float(input('nivel de soft skill: '))
-            while nota_soft < 0 or nota_soft > 10:
-                print('insira uma nota válida entre 0 e 10')
-                nota_soft = float(input('nivel de soft skill: '))
-        
-            lista_candidato.append(candidato.title())
-            lista_candidato.append(nota_entrevista) 
-            lista_candidato.append(nota_teorico)
-            lista_candidato.append(nota_pratica)
-            lista_candidato.append(nota_soft) 
-            print(f'\nAs informações do candidato(a): {candidato.title()} foram inseridas corretamente no sistema.')
+lista_candidato = ['joao',4,4,8,8],['ana',2,2,8,8]
 
-        elif opcoes == '2':
-            aprovados = listar_resultados(4,4,8,8)
-            print(aprovados)
-            
+entrevista = int(input('insira a nota da entrevista: '))
+teorico = entrevista = int(input('insira a nota do teste teorico: '))
+pratico = int(input('insira a nota dteste pratico: '))
+soft = int(input('insira a nota de soft skill: '))
+
+aprovados = listar_resultados(entrevista,teorico,pratico,soft)
+print(aprovados)
+
 # a função .join junta itens da lista e são separados pelo caractere escolhido sem a necessidade de aspas e vírgulas.
 #separador = '_'
 #juntar = separador.join(lista_candidato) 
 #print(lista_candidato)
-
-mensagem = ('Obrigado por utilizar nosso sistema :)')
-print(mensagem)
-
