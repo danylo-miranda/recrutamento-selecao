3cc27e045e9dd592af0b68318036878af4a49ef8
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-15 18:09:48 -0300

    Update 1.0.4
    implementação correta da função que busca apenas os candidatos aprovados de acordo com as notas de corte.

--- sistema.py
+++ sistema.py
@@ -1,19 +1,20 @@
-def listar_resultados(entrevista, teorico, pratica, soft):
+def listar_resultados(entrevista, teorico, pratica, soft, candidatos):
     candidatos_aprovados = []
-    for i in lista_candidato:
-        if lista_candidato[0][1] >= entrevista and lista_candidato[0][2] >= teorico and lista_candidato[0][3] >= pratica and lista_candidato[0][4] >= soft:
-            candidatos_aprovados.append(i)
+    for lista_candidato in candidatos:
+        if lista_candidato[1] >= entrevista and lista_candidato[2] >= teorico and lista_candidato[3] >= pratica and lista_candidato[4] >= soft:
+            candidatos_aprovados.append(lista_candidato)
     return candidatos_aprovados
 
-lista_candidato = ['joao',4,4,8,8],['ana',2,2,8,8]
+candidatos = [['joao', 4, 4, 8, 8], ['ana', 2, 2, 8, 8], ['silvia', 8, 8, 4, 4], ['jade', 2, 2, 4, 4], ['jackeline', 6, 6, 9, 9]]
 
-entrevista = int(input('insira a nota da entrevista: '))
-teorico = entrevista = int(input('insira a nota do teste teorico: '))
-pratico = int(input('insira a nota dteste pratico: '))
-soft = int(input('insira a nota de soft skill: '))
+entrevista = 4
+teorico = 4
+pratica = 8
+soft = 8
 
-aprovados = listar_resultados(entrevista,teorico,pratico,soft)
+aprovados = listar_resultados(entrevista, teorico, pratica, soft, candidatos)
 print(aprovados)
+
 
 # a função .join junta itens da lista e são separados pelo caractere escolhido sem a necessidade de aspas e vírgulas.
 #separador = '_'
